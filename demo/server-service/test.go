41726ce7a713934a5209e46546e8da3b72cc4f65
Your Name <you@example.com>
2024-12-10 14:28:52 +0800

    commit

--- demo/server-service/test.go
+++ demo/server-service/test.go
@@ -3,9 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
 
-	"github.com/gorilla/websocket"
 	omi "github.com/stormi-li/omiv1"
 	"github.com/stormi-li/omiv1/omihttp"
 )
@@ -18,18 +16,18 @@ func main() {
 	omiClient := omi.NewClient(options)
 	mux := omiClient.NewServeMux()
 
-	mux.HandleFunc("/http", func(w http.ResponseWriter, r *http.Request, rw *omihttp.ReadWriter) {
+	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request, rw *omihttp.ReadWriter) {
 		fmt.Fprintf(w, "hello, send by http")
 
 	})
 
-	mux.HandleFunc("/websocket", func(w http.ResponseWriter, r *http.Request, rw *omihttp.ReadWriter) {
-		upgrader := websocket.Upgrader{}
-		c, _ := upgrader.Upgrade(w, r, nil)
-		c.WriteMessage(1, []byte("hello, send by websocket"))
-		time.Sleep(100 * time.Millisecond)
-		c.Close()
-	})
+	// mux.HandleFunc("/websocket", func(w http.ResponseWriter, r *http.Request, rw *omihttp.ReadWriter) {
+	// 	upgrader := websocket.Upgrader{}
+	// 	c, _ := upgrader.Upgrade(w, r, nil)
+	// 	c.WriteMessage(1, []byte("hello, send by websocket"))
+	// 	time.Sleep(100 * time.Millisecond)
+	// 	c.Close()
+	// })
 
-	omiClient.RegisterAndServe("hello", "localhost:9014", mux)
+	omiClient.RegisterAndServe("hello_service", "localhost:9014", mux)
 }
